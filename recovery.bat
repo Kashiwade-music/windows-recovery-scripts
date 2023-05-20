3785ad3a737f6e7444dd59188c7d7a882dabaaf3
kashiwade <music5d7_9265@outlook.jp>
2023-05-20 15:39:28 +0900

    20230520_

--- recovery.bat
+++ recovery.bat
@@ -1,5 +1,4 @@
 winget install -e --id 7zip.7zip
-winget install -e --id Kitware.CMake
 winget install -e --id Discord.Discord
 winget install -e --id JGraph.Draw
 winget install -e --id GIMP.GIMP
@@ -10,3 +9,4 @@ winget install -e --id Mozilla.Thunderbird
 winget install -e --id OBSProject.OBSStudio
 winget install -e --id Microsoft.PowerShell
 winget install -e --id Microsoft.PowerToys
+winget install wez.wezterm
