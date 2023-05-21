11091b750cd6fa4b576a7f50cbd8a4171ec53cde
kashiwade <music5d7_9265@outlook.jp>
2023-05-21 10:21:22 +0900

    20230521_

--- setupWSL.ps1
+++ setupWSL.ps1
@@ -2,35 +2,11 @@
 function Install-WSL {
     # installing wsl has already been done in setup.ps1
     # wsl --install
-    # install docker
-    wsl -d Ubuntu -u root -- 'apt update'
-    wsl -d Ubuntu -u root -- 'apt install -y gnome-terminal '
-    wsl -d Ubuntu -u root -- 'apt remove docker-desktop '
-    wsl -d Ubuntu -u root -- 'rm -r $HOME/.docker/desktop'
-    wsl -d Ubuntu -u root -- 'rm /usr/local/bin/com.docker.cli'
-    wsl -d Ubuntu -u root -- 'apt purge docker-desktop'
-    wsl -d Ubuntu -u root -- 'apt remove docker docker-engine docker.io containerd runc'
-    wsl -d Ubuntu -u root -- 'apt update'
-    wsl -d Ubuntu -u root -- 'apt install -y ca-certificates curl gnupg '
-    wsl -d Ubuntu -u root -- 'install -m 0755 -d /etc/apt/keyrings'
-    wsl -d Ubuntu -u root -- 'curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg'
-    wsl -d Ubuntu -u root -- 'chmod a+r /etc/apt/keyrings/docker.gpg'
-    wsl -d Ubuntu -u root -- 'echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \'
-    wsl -d Ubuntu -u root -- 'tee /etc/apt/sources.list.d/docker.list > /dev/null'
-    wsl -d Ubuntu -u root -- 'apt update'
-    wsl -d Ubuntu -u root -- 'apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin'
-    wsl -d Ubuntu -u root -- 'docker run hello-world'
-    # install powershell
-    wsl -d Ubuntu -u root -- 'apt update'
-    wsl -d Ubuntu -u root -- 'apt-get install -y wget apt-transport-https software-properties-common'
-    wsl -d Ubuntu -u root -- 'wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"'
-    wsl -d Ubuntu -u root -- 'dpkg -i packages-microsoft-prod.deb'
-    wsl -d Ubuntu -u root -- 'rm packages-microsoft-prod.deb'
-    wsl -d Ubuntu -u root -- 'apt update'
-    wsl -d Ubuntu -u root -- 'apt install -y powershell'
-    # install homebrew
-    wsl -d Ubuntu -u root -- 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"'
-    # install oh-my-posh
-    wsl -d Ubuntu -u root -- 'brew install jandedobbeleer/oh-my-posh/oh-my-posh'
+    # copy .\setupWSL.sh to ~/setupWSL.sh in WSL Ubuntu
+    $CurrentWorkingDir = (Get-Location).Path.Replace("\", "/").Replace("C:", "/mnt/c")
+    # replace "\" to "/" in $CurrentWorkingDir
+    wsl -d Ubuntu -u root -- cp "$CurrentWorkingDir/setupWSL.sh" ~/
+    # run setupWSL.sh
+    wsl -d Ubuntu -u root -- bash ~/setupWSL.sh
 }
 Install-WSL
---
+++ setupWSL.sh
@@ -0,0 +1,31 @@
+# set up wsl
+
+apt update
+apt install -y gnome-terminal
+apt remove docker-desktop
+rm -r $HOME/.docker/desktop
+rm /usr/local/bin/com.docker.cli
+apt purge docker-desktop
+apt remove docker docker-engine docker.io containerd runc
+apt update
+apt install -y ca-certificates curl gnupg
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+echo "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" |
+    tee /etc/apt/sources.list.d/docker.list >/dev/null
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+docker run hello-world
+# install powershell
+apt update
+apt-get install -y wget apt-transport-https software-properties-common
+wget -q "https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/packages-microsoft-prod.deb"
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+apt update
+apt install -y powershell
+# install homebrew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# install oh-my-posh
+brew install jandedobbeleer/oh-my-posh/oh-my-posh
