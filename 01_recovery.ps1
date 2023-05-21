9c245de2b72618e1c2d8daa9ebdc69857e4e9fb2
kashiwade <music5d7_9265@outlook.jp>
2023-05-21 14:46:00 +0900

    20230521_

--- 01_recovery.ps1
+++ 01_recovery.ps1
@@ -10,6 +10,29 @@ function Get-YNAnswer {
     }
 }
 
+function Install-Softwares{
+    winget install -e --id 7zip.7zip
+    winget install -e --id Discord.Discord
+    winget install -e --id JGraph.Draw
+    winget install -e --id GIMP.GIMP
+    winget install -e --id Git.Git
+    winget install -e --id gerardog.gsudo
+    winget install -e --id Mozilla.Firefox
+    winget install -e --id Mozilla.Thunderbird
+    winget install -e --id OBSProject.OBSStudio
+    winget install -e --id IDRIX.VeraCrypt
+    winget install -e --id Microsoft.PowerShell
+    winget install -e --id Zoom.Zoom
+    winget install -e --id Microsoft.PowerToys
+    winget install --id Microsoft.VisualStudioCode --silent --override "/VERYSILENT /NORESTART /MERGETASKS=!runcode,addcontextmenufiles,addcontextmenufolders,associatewithfiles,addtopath"
+    winget install -e --id wez.wezterm
+    winget install JanDeDobbeleer.OhMyPosh -s winget
+    Write-Host -ForegroundColor Green "Open URL https://devtoys.app"
+    Get-YNAnswer "Have you installed devtoys?"
+}
+Install-Softwares
+
+
 function Install-Espanso {
     # open URL https://espanso.org
     Write-Host -ForegroundColor Green "Open URL https://espanso.org"
